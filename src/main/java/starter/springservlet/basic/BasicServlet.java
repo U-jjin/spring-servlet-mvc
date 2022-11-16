45854b19dbde7a999d05fcd832610b2b7ab6ed7b
U-jjin <U-jjin@githum.com>
2022-11-16 20:43:21 +0900

    [feat] servelt 구현

--- src/main/java/starter/springservlet/basic/BasicServlet.java
+++ src/main/java/starter/springservlet/basic/BasicServlet.java
@@ -1,2 +1,32 @@
-package starter.springservlet.basic;public class BasicServlet {
+package starter.springservlet.basic;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name ="basicServlet", urlPatterns = "/hello")
+public class BasicServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        System.out.println("BasicServlet.service");
+        System.out.println("request ="+request);
+        System.out.println("response ="+response);
+
+        String username =request.getParameter("username");
+        System.out.println("username: " +username);
+
+        response.setContentType("text/plain");
+        response.setCharacterEncoding("utf-8");
+        response.getWriter().write("hello "+username);
+
+    }
+
+
 }
