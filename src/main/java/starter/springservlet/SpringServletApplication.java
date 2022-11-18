888b80b8bb2058ea71712560abced0596c9d2ffe
U-jjin <U-jjin@githum.com>
2022-11-18 17:30:35 +0900

    [feat] 서블릿 구현 및 Http 요청 응답 데이터 작성 법

--- src/main/java/starter/springservlet/SpringServletApplication.java
+++ src/main/java/starter/springservlet/SpringServletApplication.java
@@ -2,7 +2,9 @@ package starter.springservlet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class SpringServletApplication {
 
